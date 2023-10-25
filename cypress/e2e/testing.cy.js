3a97c309a8bd6fbf6470b2822134302481e42f1b
aragas <gabrielarago@gmail.com>
2023-10-25 11:36:18 -0300

    testing alurapic sign up

--- cypress/e2e/testing.cy.js
+++ cypress/e2e/testing.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a', 'Register now').click()
+    cy.get('[formcontrolname="email"]').type('jimhalpert@email.com')
+    cy.get('[formcontrolname="fullName"]').type('Jim Halpert')
+    cy.get('[formcontrolname="userName"]').type('jimhalp')
+    cy.get('[formcontrolname="password"]').type('jimbo321')
+    cy.contains('button', 'Register').click()
+
   })
 })
