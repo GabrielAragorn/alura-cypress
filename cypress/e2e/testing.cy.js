0614734c530801f7203f4f67ce2ef4807057ecb0
gabriel.aragao <gabriel.aragao@tecnisys.com.br>
2023-10-26 21:58:22 -0300

    improving selectors with selector playground

--- cypress/e2e/testing.cy.js
+++ cypress/e2e/testing.cy.js
@@ -1,11 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Sign Up Page', () => {
+  it('Fill the form fields correctly to register a new user', () => {
     cy.visit('http://localhost:4200/#/home')
     cy.contains('a', 'Register now').click()
-    cy.get('[formcontrolname="email"]').type('jimhalpert@email.com')
-    cy.get('[formcontrolname="fullName"]').type('Jim Halpert')
-    cy.get('[formcontrolname="userName"]').type('jimhalp')
-    cy.get('[formcontrolname="password"]').type('jimbo321')
+    cy.get('[data-test="email"]').type('jimhalpert@email.com')
+    cy.get('[data-test="fullName"]').type('Jim Halpert')
+    cy.get('[data-test="registerUserName"]').type('jimh')
+    cy.get('[data-test="registerPassword"]').type('jimbo321')
     cy.contains('button', 'Register').click()
 
   })
