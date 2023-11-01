2a0fffc54a917efe125296bc8fa0828e80a8f38e
aragas <gabrielarago@gmail.com>
2023-11-01 16:56:54 -0300

    updated signIn correctly

--- cypress/e2e/signIn_correctly.cy.js
+++ cypress/e2e/signIn_correctly.cy.js
@@ -1,5 +1,8 @@
 describe('Sign Page', () => {
     it('Field the required fields to login successfully', () => {
-        
+        cy.visit('http://localhost:4200/#/home')
+        cy.get('[data-test="loginUserName"]').type('jimbo')
+        cy.get('[data-test="loginPassword"]').type('jimbo1234')
+        cy.contains('button', 'login').click()
     });
 })
