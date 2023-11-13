49d2fb282946fc3aec23ad575d49c1859d5f9943
aragas <gabrielarago@gmail.com>
2023-11-13 16:55:37 -0300

    updating signIn incorretly with stubs and APi interactions

--- cypress/e2e/signIn_incorrectly.cy.js
+++ cypress/e2e/signIn_incorrectly.cy.js
@@ -1,7 +1,18 @@
 describe('Sign In Page', () => {
-    it('Verify the message of required fields', () => {
+
+    beforeEach(() => {
         cy.visit('http://localhost:4200/#/home')
+        cy.intercept('POST', 'http://localhost:3000/user/login', {
+            statusCode: 401
+        }).as('stubPost') //stubPost implements a interaction direct with the API and implements the error 401
+    })
+    it('Verify the message of required fields', () => {
         cy.contains('User name is required!').should('be.visible')
         cy.contains('Password is required!').should('be.visible')
-    });
+    })
+
+    it('Must fails even when the fields is filled correctly', () => {
+        cy.login('jimhalp', 'jimbo321')
+        cy.wait('@stubPost')
+    })
 })
