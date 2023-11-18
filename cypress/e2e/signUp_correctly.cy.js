285d4c159c8bc86c91ab96868013476af50603a4
gabriel.aragao <gabriel.aragao@tecnisys.com.br>
2023-11-17 21:07:18 -0300

    added mass data to signUp / updated signUp test

--- cypress/e2e/signUp_correctly.cy.js
+++ cypress/e2e/signUp_correctly.cy.js
@@ -1,11 +1,19 @@
 describe('Sign Up Page', () => {
-  it('Fill the form fields correctly to register a new user', () => {
-    cy.visit('http://localhost:4200/#/home')
-    cy.contains('a', 'Register now').click()
-    cy.get('[data-test="email"]').type('jimhalpert@email.com')
-    cy.get('[data-test="fullName"]').type('Jim Halpert') //the get is grabing an element from the page that is tagged 
-    cy.get('[data-test="registerUserName"]').type('jimbo') //type write in the field
-    cy.get('[data-test="registerPassword"]').type('jimbo1234')
-    cy.contains('button', 'Register').click()
-  })
+
+    beforeEach(() => {
+      cy.visit('http://localhost:4200/#/home')
+    })
+
+    const users = require('../fixtures/users.json') //require the json file, so then we can use that data mass
+    users.forEach(user => {
+      it('Fill the form fields correctly to register a new user', () => {
+        cy.contains('a', 'Register now').click()
+        cy.get('[data-test="email"]').type(user.email)
+        cy.get('[data-test="fullName"]').type(user.fullName) //the get is grabing an element from the page that is tagged 
+        cy.get('[data-test="registerUserName"]').type(user.userName) //type write in the field
+        cy.get('[data-test="registerPassword"]').type(user.password)
+        cy.contains('button', 'Register').click()
+      })
+    })
+
 })
